ba32c11b4e4c68301e21b904251a49a6e5148c47
Ralf S. Engelschall <rse@engelschall.com>
2020-09-13 11:02:13 +0200

    add missing helper script

---
+++ npm-install.js
@@ -0,0 +1,14 @@
+/*
+**  Live Video Experience (LiVE)
+**  Copyright (c) 2020 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Licensed under GPL 3.0 <https://spdx.org/licenses/GPL-3.0-only>
+*/
+
+const os = require("os")
+const fs = require("fs")
+
+if (os.platform() === "darwin")
+    fs.chmodSync("app-main-relay-videostream.d/ffmpeg", 0o755)
+else if (os.platform() === "win32")
+    fs.chmodSync("app-main-relay-videostream.d/ffmpeg.exe", 0o755)
+
