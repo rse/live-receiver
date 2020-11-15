6f470fd656e08ea3009b477ae4649706eb949416
Ralf S. Engelschall <rse@engelschall.com>
2020-11-15 11:28:54 +0100

    improve 'npm install' procedure by automatically creating necessary sub-directory

--- npm-install.js
+++ npm-install.js
@@ -4,11 +4,19 @@
 **  Licensed under GPL 3.0 <https://spdx.org/licenses/GPL-3.0-only>
 */
 
-const os = require("os")
-const fs = require("fs")
+const os     = require("os")
+const fs     = require("fs")
+const mkdirp = require("mkdirp")
 
-if (os.platform() === "darwin" || os.platform() === "linux")
-    fs.chmodSync("app-main-relay-videostream.d/ffmpeg", 0o755)
-else if (os.platform() === "win32")
-    fs.chmodSync("app-main-relay-videostream.d/ffmpeg.exe", 0o755)
+if (process.argv[2] === "prolog") {
+    /*  prolog: actions before npm-install-fetch  */
+    mkdirp.sync("app-main-relay-videostream.d", { mode: 0o755 })
+}
+else if (process.argv[2] === "epilog") {
+    /*  epilog: actions after NPM-install-fetch  */
+    if (os.platform() === "darwin" || os.platform() === "linux")
+        fs.chmodSync("app-main-relay-videostream.d/ffmpeg", 0o755)
+    else if (os.platform() === "win32")
+        fs.chmodSync("app-main-relay-videostream.d/ffmpeg.exe", 0o755)
+}
 
