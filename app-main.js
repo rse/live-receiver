f8e56c0a562937346e5943a7943ee30803e70001
Ralf S. Engelschall <rse@engelschall.com>
2022-01-04 12:36:20 +0100

    bump year in all copyright messages

--- app-main.js
+++ app-main.js
@@ -1,6 +1,6 @@
 /*
 **  Live Video Experience (LiVE)
-**  Copyright (c) 2020-2021 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2020-2022 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **  Licensed under GPL 3.0 <https://spdx.org/licenses/GPL-3.0-only>
 */
 
